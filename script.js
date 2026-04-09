2102a64f182669e9c9404e1dd05429ff9e065ae2
anantshukla-1 <shukla18anant9839@gmail.com>
2026-04-09 14:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,35 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
+	console.log("button clicked");
 //Add your code here
+	let priceElements = document.querySelectorAll('.price');
+	let totalPrice = 0;
+	
+	priceElements.forEach((elements)=>{
+		let price = parseFloat(elements.textContent);
+		if(!isNaN(price)){
+			totalPrice += price;
+		}
+	});
+
+	const table = document.querySelector('table');
+	let existingTotal = document.querySelector('#total-row');
+	if(existingTotal) {
+		existingTotal.remove(); 
+	    console.log("Updated existing row");
+	};
+	
+	const newRow = document.createElement('tr');
+	newRow.id = 'total-row';
+	const newCell = document.createElement('td');
+
+	newCell.textContent = `Total Price: ${totalPrice}`;
+	newCell.setAttribute('colspan' , '2');
+	newCell.style.fontWeight = 'bold';
+
+	newRow.appendChild(newCell);
+	table.appendChild(newRow);
   
 };
 
